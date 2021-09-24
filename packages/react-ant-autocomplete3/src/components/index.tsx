be519d0f93c647b10e955c3164feda31cc099c85
afeiship <1290657123@qq.com>
2021-09-24 16:32:49 +0800

    feat: migrate with query

--- packages/react-ant-autocomplete3/src/components/index.tsx
+++ packages/react-ant-autocomplete3/src/components/index.tsx
@@ -13,64 +13,152 @@ interface EventTarget {
   };
 }
 
-export type ReactAntAutocomplete3Props = Omit<AutoCompleteProps, 'onChange' | 'onSearch'> & {
+interface QueryResult {
+  items: any[];
+  total: number;
+}
+
+interface TemplateArgs {
+  item: any;
+  index: number;
+}
+
+type OverrideProps = 'value' | 'onChange' | 'onSearch' | 'onSelect';
+
+const DEFAULT_TEMPLATE = ({ item }) => {
+  return item.label;
+};
+
+export type ReactAntAutocomplete3Props = Omit<AutoCompleteProps, OverrideProps> & {
   /**
    * The extended className for component.
    */
   className?: string;
   /**
+   * The keyword.
+   */
+  value?: string;
+  /**
    * The data source list.
    */
   items?: any[];
   /**
+   * The item template.
+   * @param args
+   */
+  template?: (args: TemplateArgs) => React.ReactNode;
+  /**
    * The change handler.
+   * @param event
    */
   onChange?: (event: EventTarget) => void;
   /**
    * The handler when search.
+   * @param event
    */
   onSearch?: (event: EventTarget) => void;
+  /**
+   * The handler when select.
+   * @param event
+   */
+  onSelect?: (event: EventTarget) => void;
   /**
    * The debounce delay interval.
    */
   interval?: number;
+  /**
+   * The api for query records.
+   * @param keyword
+   */
+  query?: (keyword?: string) => Promise<QueryResult> | unknown;
 };
 
 export default class ReactAntAutocomplete3 extends Component<ReactAntAutocomplete3Props> {
   static displayName = CLASS_NAME;
   static version = '__VERSION__';
   static defaultProps = {
+    value: '',
+    items: [],
+    template: DEFAULT_TEMPLATE,
     interval: 100,
     onChange: noop,
-    onSearch: noop
+    onSearch: noop,
+    onSelect: noop
   };
+
+  private mounted = false;
+
+  state = {
+    value: this.props.value,
+    items: this.props.items
+  };
+
+  private doQuery = () => {
+    const { value } = this.state;
+    const { query } = this.props;
+    const kw = value || '';
+    if (query) {
+      // @ts-ignore
+      return query(kw).then(({ items }) => {
+        if (!this.mounted) return;
+        this.setState({ items });
+      });
+    }
+  };
+
+  get items() {
+    const { items } = this.state;
+    const { template } = this.props;
+    if (template) {
+      return items!.map((item, index) => {
+        return template({ item, index });
+      });
+    }
+    return items;
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    this.doQuery();
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
 
   handleSearch = debounce((inEvent) => {
     const { onSearch } = this.props;
-    onSearch!({
-      target: { value: inEvent }
-    });
+    const value = inEvent;
+    this.doQuery();
+    onSearch!({ target: { value } });
   }, this.props.interval);
 
   handleChange = (inEvent) => {
     const { onChange } = this.props;
-    onChange!({
-      target: { value: inEvent }
-    });
+    const target = { value: inEvent };
+    this.setState(target);
+    onChange!({ target });
+  };
+
+  handleSelect = (inEvent) => {
+    const { onSelect } = this.props;
+    onSelect!({ target: { value: inEvent } });
   };
 
   render() {
-    const { className, onChange, onSearch, items, ...props } = this.props;
-
+    const { className, onChange, onSearch, onSelect, items, query, value, ...props } = this.props;
+    const _value = this.state.value;
     return (
       <AutoComplete
         data-component={CLASS_NAME}
         className={classNames(CLASS_NAME, className)}
-        dataSource={items}
+        value={_value}
+        dataSource={this.items}
         showSearch
         allowClear
         onSearch={this.handleSearch}
         onChange={this.handleChange}
+        onSelect={this.handleSelect}
         {...props}
       />
     );
--- packages/websites/src/App.tsx
+++ packages/websites/src/App.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 import ReactAntAutoComplete from "@jswork/react-ant-autocomplete3";
+// @ts-ignore
+import nx from "@jswork/next";
+import "@jswork/next-key-map";
+// import { useEffect } from "react";
 
 const Container = styled.div`
   border: 1px solid #ccc;
@@ -7,15 +11,36 @@ const Container = styled.div`
   padding: 10px;
 `;
 
+const headers = {
+  "Content-Type": "application/json",
+  authorization:
+    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1dHlwIjoiTERBUF9VU0VSIiwiYXVkIjpbImFvc3AtZ2F0ZXdheSIsIml5eS1hcGkiLCJhb3QiLCJ2d29wcyIsIm5vdGlmaWNhdGlvbi1jZW50ZXItYWRtaW4iLCJhb2MtYWRtaW4iXSwic3ViIjoiYXJpYy56aGVuZyIsImlzcyI6ImFsbzcuY29tIiwiZXhwIjoxNjMyNDg1OTI4LCJqdGkiOiIyZTZlZWM3OC1jMTkyLTQ1MTEtYmIxMi0xNTA0YTk2Y2QyOGQifQ.CJOZ_r2TEkl3bZEqsH8VDeXcum0UHnorl21Ir-o8UK9RyipB1eFZYf0obfkDglyWC-2rHnPMmpW5VgHhDGWOCwPwM503-u0KBVkk5yhsCWnvEyBEwNoOn_k-auOd7NsWHoiT4z5-GnABMAlWe5e3BlO_3InzoBueMvatEY6tidM",
+  baseinfo: "0,6847",
+};
+
 function App() {
+  const query = (keyword?: string) => {
+    return fetch(
+      `https://aosp-gateway.beta.saybot.net/ace-kellis/api/v1/course_series_english?pageNo=1&pageSize=10&name=${keyword}`,
+      { headers }
+    )
+      .then((r) => r.json())
+      .then((res) => {
+        const items = nx.keyMap(res.result, {
+          uuid: "value",
+          cnName: "label",
+        });
+        console.log("items:", items);
+        return { items, total: res.totalCount };
+      });
+  };
+
   return (
     <Container className="App">
       <ReactAntAutoComplete
         placeholder="请输入关键字进行搜索"
         style={{ width: 200 }}
-        onChange={(e) => {
-          console.log("e:", e.target.value);
-        }}
+        query={query}
         onSearch={(e) => {
           console.log("search e:", e.target.value);
         }}
